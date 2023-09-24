77989640c4307bba31c3bdfa88ee0c45bb206a85
MasayukiHorimoto <141377771+snow-smile1201@users.noreply.github.com>
2023-09-24 02:35:26 +0000

    [Add]books/index books/show ビューにいいね機能実装

--- config/routes.rb
+++ config/routes.rb
@@ -2,13 +2,14 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   root to: 'homes#top'
-  get "home/about"=>"homes#about"
+  get "home/about"=>"homes#about", as: 'about'
   devise_for :users
   devise_scope :user do
     get '/users/sign_out' => 'devise/sessions#destroy'
   end
-  resources :books, only: [:index,:show,:edit,:create,:destroy,:update]
+  resources :books, only: [:index,:show,:edit,:create,:destroy,:update] do
+   resource :favorites, only: [:create, :destroy]
+ end
   resources :users, only: [:index,:show,:edit,:update]
-
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
