62559f7738a5159cc9665e9d2f015e8c88c06a09
MasayukiHorimoto <141377771+snow-smile1201@users.noreply.github.com>
2023-09-27 08:51:23 +0000

    [Add]bookの投稿数、前日比、前週比実装、コメントの非同期化実装途中

--- app/controllers/book_comments_controller.rb
+++ app/controllers/book_comments_controller.rb
@@ -5,13 +5,11 @@ class BookCommentsController < ApplicationController
     comment.user_id = current_user.id
     comment.book_id = book.id
     comment.save
-    redirect_to book_path(book)
   end
   def destroy
     book = Book.find(params[:book_id])
     comment = BookComment.find(params[:id])
     comment.destroy
-    redirect_to book_path(book)
   end
 
   private
