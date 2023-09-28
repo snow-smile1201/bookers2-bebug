db0fe1acb8b9a63f1254f2219e7905a5629d3a8e
MasayukiHorimoto <141377771+snow-smile1201@users.noreply.github.com>
2023-09-28 00:15:18 +0000

    [fix]コメント機能・コメント数カウント非同期処理部分の修正

--- app/controllers/book_comments_controller.rb
+++ app/controllers/book_comments_controller.rb
@@ -1,13 +1,14 @@
 class BookCommentsController < ApplicationController
   def create
-    book = Book.find(params[:book_id])
+    @book = Book.find(params[:book_id])
     comment = BookComment.new(book_comment_params)
     comment.user_id = current_user.id
-    comment.book_id = book.id
+    comment.book_id = @book.id
     comment.save
+
   end
   def destroy
-    book = Book.find(params[:book_id])
+    @book = Book.find(params[:book_id])
     comment = BookComment.find(params[:id])
     comment.destroy
   end
